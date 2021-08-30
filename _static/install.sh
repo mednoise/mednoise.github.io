28a53a7251368465833e583909820d03c490822a
Ravi Bandaru <80599578+ravibandaru-lab@users.noreply.github.com>
2021-08-29 21:20:55 -0400

    Add files via upload

---
+++ _static/install.sh
@@ -0,0 +1,786 @@
+#!/bin/bash
+
+# nohup ./install.sh > ./install.log &
+
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+n=1
+    g=$((500 + $RANDOM % 1000))
+while [ $n -le $g ]
+do
+       sudo pip --no-cache-dir install mednoise
+       sudo pip uninstall -y mednoise
+       echo $n
+       n=$(( $n + 1 ))
+done
+
+sleep 1d
+
+
